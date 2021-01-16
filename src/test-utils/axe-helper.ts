f634c4248661590a2d625cffc6850d9794f21dc5
Gopal Saini <gopal.saini.work@gmail.com>
2021-01-16 11:14:09 -0500

    Added React AXE for Accessibility Testing

--- src/test-utils/axe-helper.ts
+++ src/test-utils/axe-helper.ts
@@ -1,5 +1,7 @@
 import { configureAxe } from "jest-axe";
 
+// https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
+
 export const axe = configureAxe({
   rules: {
     // for demonstration only, don't disable rules that need fixing.
